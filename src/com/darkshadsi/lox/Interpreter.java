b224e7c33da2824daaa643ff7e028db7eed36d56
DarkShadsi <depaudhonvincent24@gmail.com>
2026-06-25 20:14:26 +0800

    Implemented variable scopes.
    - The program can now distinguish local and global variables.

--- src/com/darkshadsi/lox/Interpreter.java
+++ src/com/darkshadsi/lox/Interpreter.java
@@ -92,6 +92,22 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         stmt.accept(this);
     }
 
+    void executeBlock(List<Stmt> statements, Environment environment) {
+        Environment previous = this.environment;
+        try{
+            for(Stmt statement: statements){
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
         evaluate(stmt.expression);
