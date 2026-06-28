b4dec9ed780fd0f015496912220264837ac97f62
DarkShadsi <depaudhonvincent24@gmail.com>
2026-06-28 19:20:41 +0800

    Added support for local functions and closures.

--- src/com/darkshadsi/lox/Parser.java
+++ src/com/darkshadsi/lox/Parser.java
@@ -151,6 +151,7 @@ class Parser {
     }
 
     private Stmt.Function function(String kind) {
+        //TODO: allow lambdas
         Token name = consume(IDENTIFIER, "Expect " + kind + " name.");
         consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
         List<Token> parameters = new ArrayList<>();
