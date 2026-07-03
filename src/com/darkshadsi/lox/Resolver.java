5c668d6b36f7a977c4a1c3c82bc32bc67cbe14cc
DarkShadsi <depaudhonvincent24@gmail.com>
2026-07-03 20:33:19 +0800

    Binded the 'super' keyword
    - the interpreter nopw correctly identify which superclass method is being referred.

--- src/com/darkshadsi/lox/Resolver.java
+++ src/com/darkshadsi/lox/Resolver.java
@@ -56,6 +56,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
             resolve(stmt.superclass);
         }
 
+        if (stmt.superclass != null) {
+            beginScope();
+            scopes.peek().put("super", true);
+        }
+
         beginScope();
         scopes.peek().put("this", true);
 
@@ -68,6 +73,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         endScope();
+        if (stmt.superclass != null) endScope();
 
         currentClass = enclosingClass;
         return null;
@@ -159,6 +165,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitSetExpr(Expr.Set expr) {
         resolve(expr.value);
         resolve(expr.object);
+        return null;
+    }
+
+    @Override
+    public Void visitSuperExpr(Expr.Super expr) {
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
