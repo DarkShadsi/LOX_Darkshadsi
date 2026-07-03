1c524054abd50cd2d30166df1ad080293289329f
DarkShadsi <depaudhonvincent24@gmail.com>
2026-07-03 20:14:59 +0800

    Added super keyword.
    - subclasses can now directly access superclass' methods (if it was overridden) through the super keyword.

--- src/com/darkshadsi/lox/Parser.java
+++ src/com/darkshadsi/lox/Parser.java
@@ -338,25 +338,32 @@ class Parser {
     }
 
     private Expr primary() {
-        if(match(FALSE)) return new Expr.Literal(false);
-        if(match(TRUE)) return new Expr.Literal(true);
-        if(match(NIL)) return new Expr.Literal(null);
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
 
-        if(match(NUMBER, STRING)) {
+        if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
         }
 
-        if(match(LEFT_PAREN)) {
+        if (match(SUPER)) {
+            Token keyword = previous();
+            consume(DOT, "Expect '.' after 'super'.");
+            Token method = consume(IDENTIFIER, "Expect superclass method name.");
+            return new Expr.Super(keyword, method);
+        }
+
+        if (match(LEFT_PAREN)) {
             Expr expr = expression();
             consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
         }
 
-        if(match(THIS)) {
+        if (match(THIS)) {
             return new Expr.This(previous());
         }
 
-        if(match(IDENTIFIER)) {
+        if (match(IDENTIFIER)) {
             return new Expr.Variable(previous());
         }
 
