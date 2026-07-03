23afe3dd4699f27f3f4e7f5da25a83fc23f6bdf0
DarkShadsi <depaudhonvincent24@gmail.com>
2026-07-03 20:41:18 +0800

    Added error handling for the 'super' keyword semantics

--- src/com/darkshadsi/lox/Resolver.java
+++ src/com/darkshadsi/lox/Resolver.java
@@ -24,7 +24,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
 
     void resolve(List<Stmt> statements) {
@@ -53,6 +54,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         if (stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
         }
 
@@ -170,6 +172,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword, "Can't use 'super' in a class without a superclass.");
+        }
         resolveLocal(expr, expr.keyword);
         return null;
     }
